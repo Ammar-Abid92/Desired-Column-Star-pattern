f846cdb43245477ecccb4b5ce476e96d44240fb1
Ammar-Abid92 <72232879+Ammar-Abid92@users.noreply.github.com>
2020-11-11 23:00:24 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# program for star pattern
+def stars(pattern):
+    for pattern in range(7, 0, -1):
+        for lower_pattern in range(pattern-1):
+            print("*", end=" ")
+        print("*")
+    return stars
+
+
+stars(404)
+
+print(stars(404))
+
+
+
+
+
